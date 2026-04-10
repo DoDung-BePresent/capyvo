8806bf1b35dc8f30f57ab36eef6c3a685b48f6ae
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2026-04-10 22:16:45 +0700

    feat: add network status components and enhance layouts with scroll-to-top functionality

--- client/src/features/exam/layouts/UserLayout.tsx
+++ client/src/features/exam/layouts/UserLayout.tsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 import { Layout } from 'antd'
 import { UserSidebar, UserHeader, UserContent, UserFooter } from './components'
+import { NetworkStatusBanner } from '@/shared/components'
 
 export default function UserLayout() {
   const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <Layout hasSider style={{ height: '100vh', overflow: 'hidden' }}>
-      <UserSidebar collapsed={collapsed} onCollapse={setCollapsed} />
-      <Layout>
-        <UserHeader collapsed={collapsed} onCollapse={setCollapsed} />
-        <UserContent />
-        <UserFooter />
+    <>
+      <NetworkStatusBanner />
+      <Layout hasSider style={{ height: '100vh', overflow: 'hidden' }}>
+        <UserSidebar collapsed={collapsed} onCollapse={setCollapsed} />
+        <Layout>
+          <UserHeader collapsed={collapsed} onCollapse={setCollapsed} />
+          <UserContent />
+          <UserFooter />
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   )
 }
